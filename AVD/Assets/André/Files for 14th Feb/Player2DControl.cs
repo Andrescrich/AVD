72b302eca62390020fed9ef38fc4c807782617ae
André <andrescrich@hotmail.com>
2020-02-14 19:31:39 +0100

    14Feb

---
+++ AVD/Assets/André/Files for 14th Feb/Player2DControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class Player2DControl : MonoBehaviour
+{
+    public float speed = 20f;
+    private float horizontalMove;
+    private bool jump;
+    private bool crouch;
+    private CharacterController2D controller;
+    private Animator anim;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController2D>();
+        anim = GetComponent<Animator>();
+    }
+    
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        anim.SetFloat("Speed", Math.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            anim.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+    }
+
+    public void OnLanding()
+    {
+        anim.SetBool("IsJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
